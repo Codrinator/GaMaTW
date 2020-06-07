203801c73b2fc4478ca6e448ff53fac100707472
Dominte <59875175+Dominte@users.noreply.github.com>
2020-06-07 16:11:47 +0300

    admin Middleware

--- Assets/Scripts/admin.js
+++ Assets/Scripts/admin.js
@@ -1,15 +1,16 @@
 function processRequest() {
     const url = '/api/admin/registerGame';
     const request = new XMLHttpRequest();
-
-
+    const error = document.getElementById('errorHTML');
     request.open('POST', url);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
     request.responseType = 'json';
     request.onload = function () {
         if (request.response.success === true) {
             alert(request.response.status);
-        }
+        } else
+            error.innerHTML = request.response.status;
     };
     const formData = new FormData(form);
 
--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -2,33 +2,55 @@ const Game = require('../Models/index').Game;
 
 const insertGame = async function insertGame(req, res) {
 
-    const name= req.body.name;
-    const company= req.body.company;
-    const genres= req.body.genres;
-    const price=req.body.price;
-    const category= req.body.category;
-    const release_date = req.body.release_date;
-    const age_restriction= req.body.age_restriction;
-    const platform= req.body.platform;
-    const game_description= req.body.description;
-
-    Game.Game({
-        name: name,
-        release_date:release_date,
-        company:company,
-        genres:genres,
-        price:price,
-        category:category,
-        age_restriction:age_restriction,
-        platform:platform,
-        game_description:game_description
-    }).save(function (err) {
-        if (err) throw err;
+    if (req.user.payload !== 'admin') {
         res.end(JSON.stringify({
-            success: true,
-            status: 'Register complete'
+            success: false,
+            status: 'You do not have this permission'
         }))
-    });
+    }
+    const price = req.body.price;
+    const release_date = req.body.release_date;
+    const regexNumber = /^[0-9]+$/;
+    const regexDate = /^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/;
+    if (regexDate.test(release_date) === false) {
+        res.end(JSON.stringify({
+            success: 'false',
+            status: 'Wrong date Format'
+        }));
+    } else if (regexNumber.test(price) === false) {
+        res.end(JSON.stringify({
+            success: 'false',
+            status: 'Price has to be a number'
+        }));
+    } else {
+
+        const name = req.body.name;
+        const company = req.body.company;
+        const genres = req.body.genres;
+        const category = req.body.category;
+        const age_restriction = req.body.age_restriction;
+        const platform = req.body.platform;
+        const game_description = req.body.description;
+
+
+        Game.Game({
+            name: name,
+            release_date: release_date,
+            company: company,
+            genres: genres,
+            price: price,
+            category: category,
+            age_restriction: age_restriction,
+            platform: platform,
+            game_description: game_description
+        }).save(function (err) {
+            if (err) throw err;
+            res.end(JSON.stringify({
+                success: true,
+                status: 'Register complete'
+            }))
+        });
+    }
 };
 
 module.exports = {insertGame};
