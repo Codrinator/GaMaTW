785c0181cfaff2cd9a0f4015d18d982ed82c1b08
Codrin Donciu <33196995+Codrinator@users.noreply.github.com>
2020-06-08 00:41:45 +0300

    Fix what i broke

--- Routes/collection.js
+++ Routes/collection.js
@@ -1,9 +1,9 @@
-const collection = require('../Controllers/index').collection;
+const collection = require('../Controllers/index');
 const Router = require('router');
 
 const gameCollection = Router();
 
-gameCollection.post('/loadCollection', collection);
+gameCollection.post('/loadCollection', collection.collection);
 gameCollection.get('/downloadStatistic', collection.getStatistic);
 gameCollection.get('/downloadCSV', collection.getCSV);
 
